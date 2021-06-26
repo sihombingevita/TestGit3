2a958627cf744f22bd9b065ec6e2b5c86f30a7a5
Evita <veronikaevita@gmail.com>
2021-06-26 11:23:53 +0700

    Initial Commit

---
+++ TestGit3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGit3
+//
+//  Created by Evita Sihombing on 26/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
